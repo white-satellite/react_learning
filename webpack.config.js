42d4a97a61dc42c4947c45c6db26963ee35b4b55
white-satellite <masayukiuminin8@gmail.com>
2023-04-25 20:15:54 +0900

    初期設定_最初のページを開けるようにした

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: './',
+    },
+    port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
